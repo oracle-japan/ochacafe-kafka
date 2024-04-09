3867494b40a1e6d0eb171dcec79427f1285ee55a
shukawam <shukawam@gmail.com>
2024-04-09 23:55:29 +0900

    Update using jul logger

--- kafka-java-client-example/src/main/java/me/shukawam/consumer/ConsumerMain.java
+++ kafka-java-client-example/src/main/java/me/shukawam/consumer/ConsumerMain.java
@@ -3,12 +3,14 @@ package me.shukawam.consumer;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.logging.Logger;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class ConsumerMain {
+    private static final Logger logger = Logger.getLogger(ConsumerMain.class.getName());
+
     public static void main(String[] args) {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9092");
@@ -21,9 +23,10 @@ public class ConsumerMain {
         consumer.subscribe(Arrays.asList("my-topic"));
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
-            }
+            records.forEach(record -> {
+                logger.info(String.format("offset = %d, key = %s, value = %s%n", record.offset(), record.key(),
+                        record.value()));
+            });
         }
     }
 }
